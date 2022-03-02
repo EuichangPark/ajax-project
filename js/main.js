a5c96df4091fec2969a991aa7c9cedb11582ca94
Euichang Park <euichangpark@gmail.com>
2022-03-02 14:09:26 -0500

    empty css rule set deleted.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 var xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://digimon-api.vercel.app/api/digimon' + name);
+xhr.open('GET', 'https://digimon-api.vercel.app/api/digimon');
 xhr.responseType = 'json';
 var imageSrcArray = [];
+var nameArray = [];
+var levelArray = [];
 xhr.addEventListener('load', function () {
   for (var i = 0; i < xhr.response.length; i++) {
     imageSrcArray.push(xhr.response[i].img);
+    nameArray.push(xhr.response[i].name);
+    levelArray.push(xhr.response[i].level);
   }
 });
 xhr.send();
@@ -25,8 +29,39 @@ function nextImage() {
 
 var $searchNavbar = document.querySelector('#search-nav');
 var $searchBox = document.querySelector('.search-box');
+var $noMessage = document.querySelector('.not-in-library');
+var $valueOfSearch = '';
+
 $searchNavbar.addEventListener('click', searchNav);
 function searchNav(event) {
   $intervalImgDiv.className = 'hidden';
   $searchBox.className = 'search-box';
+}
+
+var $inLibraryDiv = document.querySelector('.in-library');
+var $iconBtn = document.querySelector('#search-icon');
+$iconBtn.addEventListener('click', initiateSearch);
+function initiateSearch(event) {
+  $valueOfSearch = document.querySelector('#search-area').value;
+  for (var i = 0; i < nameArray.length; i++) {
+    if ($valueOfSearch.toLowerCase() === nameArray[i].toLowerCase()) {
+      var searchResult = document.querySelector('.search-img-result');
+      if (!searchResult) {
+        var $digimonName = document.createElement('h3');
+        $digimonName.textContent = nameArray[i];
+        $inLibraryDiv.appendChild($digimonName);
+        var $inLibraryImg = document.createElement('img');
+        $inLibraryImg.classList.add('search-img-result');
+        $inLibraryImg.setAttribute('src', imageSrcArray[i]);
+        $inLibraryDiv.appendChild($inLibraryImg);
+        var $digimonLevel = document.createElement('h3');
+        $digimonLevel.textContent = levelArray[i];
+        $inLibraryDiv.appendChild($digimonLevel);
+      }
+    } else {
+      $intervalImgDiv.className = 'hidden';
+      $noMessage.className = 'not-in-library';
+
+    }
+  }
 }
